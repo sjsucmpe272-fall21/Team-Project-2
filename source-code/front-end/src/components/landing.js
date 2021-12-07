dd795fef00fac62ab333495ca6bc08e7193d2aa4
omkarghule27 <omkarshivaji.ghule@sjsu.edu>
2021-12-06 20:54:40 -0800

    deply changes 6

--- source-code/front-end/src/components/landing.js
+++ source-code/front-end/src/components/landing.js
@@ -241,7 +241,7 @@ alert('Inside this error222');
     const setB = answer.userskills.split(" ");
     function difference(setB, setA){
     let _difference = new Set(setA)
-    for (let elem of setB\) {
+    for (let elem of setB) {
         _difference.delete(elem)
     }
     return _difference
