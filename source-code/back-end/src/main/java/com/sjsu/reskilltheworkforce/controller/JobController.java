15e3dbedfe863e111f65077c2366f7ac958f74c5
omkarghule27 <90274199+omkarghule27@users.noreply.github.com>
2023-04-05 18:01:41 -0700

    Update JobController.java

--- source-code/back-end/src/main/java/com/sjsu/reskilltheworkforce/controller/JobController.java
+++ source-code/back-end/src/main/java/com/sjsu/reskilltheworkforce/controller/JobController.java
@@ -18,65 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-@CrossOrigin
-@RestController
-@Slf4j
-@RequestMapping("/api/jobs")
-public class JobController {
-
-    @Autowired
-    private JobService jobService;
-
-    @PostMapping("/get-jobs-by-allparams")
-    public ResponseEntity<?> getJobsByAllParams(@RequestBody JobParams jobParams) throws Exception {
-        log.info("Entering getJobsByAllParams Api");
-        Object resp = null;
-        try {
-            log.info("Input to getJobsByAllParams API is:{}",jobParams);
-            resp = jobService.getJobs(jobParams);
-            if(resp!=null && resp.getClass().equals(AsyncParams.class)){
-                jobService.doAsyncScrape((AsyncParams) resp);
-                resp = ((AsyncParams) resp).getToken();
-            }
-        } catch (Exception e) {
-            log.error("Error occured in getJobsByAllParams :{}", e);
-            return ResponseEntity.status(400).body(e.getMessage());
-        } finally {
-            log.info("Exiting getJobsByAllParams Api");
-        }
-        return ResponseEntity.ok(resp);
-    }
-
-
-    @GetMapping("/get-jobs-by-token")
-    public ResponseEntity<?> getJobsByToken(@RequestParam String token) throws Exception {
-        log.info("Entering getJobsByToken Api");
-        Object s = null;
-        try {
-            log.info("Input to getJobsByToken API is:{}",token);
-            s = jobService.getJobsByToken(token);
-        } catch (Exception e) {
-            log.error("Error occured in getJobsByToken :{}", e);
-            return ResponseEntity.status(400).body(e.getMessage());
-        } finally {
-            log.info("Exiting getJobsByToken Api");
-        }
-        return ResponseEntity.ok(s);
-    }
-
-    @PostMapping("/get-jobs-by-allparams3")
-    public ResponseEntity<?> getJobsByAllParams3() throws Exception {
-        log.info("Entering getJobsByAllParams Api3");
-        List<JobPostings> s = null;
-        try {
-            jobService.s3();
-        } catch (Exception e) {
-            log.error("Error occured in getJobsByAllParams :{}", e);
-            return ResponseEntity.status(400).body(e.getMessage());
-        } finally {
-            log.info("Exiting getJobsByAllParams Api");
-        }
+ }
         return ResponseEntity.ok(s);
     }
 }
